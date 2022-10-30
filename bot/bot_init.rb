dd595f01745e7e27632a25c25fc988defc5c5196
Zuvvo <Zuvvo4@gmail.com>
2022-10-30 14:44:10 +0100

    time test

--- bot/bot_init.rb
+++ bot/bot_init.rb
@@ -3,7 +3,7 @@ require 'discordrb'
 bot = Discordrb::Bot.new token: ENV['DISCORD_TOKEN']
 
 bot.message(with_text: 'Ping!') do |event|
-  event.respond 'Pong production!'
+  event.respond "Pong production! #{Time.new}"
 end
 
 bot.run
